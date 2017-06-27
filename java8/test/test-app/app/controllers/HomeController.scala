b5c3e98f2a98c35daadbb8e66a7ccae2a19bd3dc
Bob <bobgrijpstra@gmail.com>
2017-06-27 15:33:44 +0200

    welcome

--- java8/test/test-app/app/controllers/HomeController.scala
+++ java8/test/test-app/app/controllers/HomeController.scala
@@ -18,7 +18,7 @@ class HomeController @Inject() extends Controller {
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index("Hi Yoran!"))
+    Ok(views.html.index("Welcome to my empty scala app!"))
   }
 
 }
